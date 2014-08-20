e6137a917636a8460442cbccf30866cc1aff36fc
monodo <monodo@WSSGRF030.ne.ch>
2014-08-20 09:47:52 +0200

    Set up objects for dom cleaning

---
+++ resources/js/pointscope.js
@@ -0,0 +1,34 @@
+/**
+ * POintScope namespace.
+ */
+if (typeof PointScope == "undefined") {
+  PointScope = {
+  
+    /**
+     * Initializes this object.
+     */
+    Renderer:  {},
+	
+    /**
+     * Bootstrap interface (interface is a js reserved keyword).
+     */	
+	PsInterface: {},
+	
+	/**
+     * Las / PoinCloud readers.
+     */
+	Readers: {},
+	
+	/**
+     * Projections definitions.
+     */
+	Projections: {},
+	
+	/**
+     * Colormaps for point symbology.
+     */
+	Colormaps: {}
+	
+  };
+
+};
